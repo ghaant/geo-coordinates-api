473b8629b2c58ffe97519a47b9d34fa686b0e80c
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2020-03-12 00:29:05 +0100

    Fix response statuses.

--- app/controllers/geo_coordinates_controller.rb
+++ app/controllers/geo_coordinates_controller.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-require './lib/locationiq/connection.rb'
-
 class GeoCoordinatesController < ApplicationController
   def locate
     if params[:token].nil?
       response.status = 406
-      render(json: { message: 'Please provide your access token.', status: response.status }) && return
+      render(json: { message: 'Please provide your access token.' }) && return
     elsif params[:search_value].nil?
       response.status = 406
       render(json: { message: 'Please input the search value: an address or a name of a place you are searching for.', status: response.status }) && return
     end
 
-    response = Locationiq::Client.new.coordinates(params[:token], params[:search_value])
+    client_response = Locationiq::Client.new.coordinates(params[:token], params[:search_value])
 
-    if response.status == 401
-      render(json: { message: 'There is a problem with your access token.', status: response.status }) && return
-    elsif response.status == 404
-      render(json: { message: 'Nothing found. Please check the search value and try again.', status: response.status }) && return
-    elsif response.status == 200 && JSON.parse(response.body).length > 1
+    if client_response.status == 401
+      response.status = client_response.status
+      render(json: { message: 'There is a problem with your access token.' }) && return
+    elsif client_response.status == 404
+      response.status = client_response.status
+      render(json: { message: 'Nothing found. Please check the search value and try again.' }) && return
+    elsif client_response.status == 200 && JSON.parse(client_response.body).length > 1
       response.status = 400
-      render(json: { message: 'There are more than one result for this request. Please put it more specificly.', status: response.status }) && return
-    elsif response.status != 200
+      render(json: { message: 'There are more than one result for this request. Please put it more specificly.' }) && return
+    elsif client_response.status != 200
       response.status = 500
-      render(json: { message: 'There is something wrong with LocationIQ service. Please try again later.', status: response.status }) && return
+      render(json: { message: 'There is something wrong with LocationIQ service. Please try again later.' }) && return
     end
 
     location = JSON.parse(response.body).first
