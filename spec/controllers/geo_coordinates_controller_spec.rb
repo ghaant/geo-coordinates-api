ef4e21061cd07e098b88c243cdc49d6b090a59b0
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2020-03-12 00:32:30 +0100

    Add a test for the wrong token.

--- spec/controllers/geo_coordinates_controller_spec.rb
+++ spec/controllers/geo_coordinates_controller_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe GeoCoordinatesController, type: :controller do
     expect(response).to have_http_status(406)
     expect(JSON.parse(response.body)['message']).to eq('Please input the search value: an address or a name of a place you are searching for.')
   end
+
+  it 'returns 401 in case of the wrong token' do
+    get 'locate', params: { token: 'asdf', search_value: 'blah-blah' }
+
+    expect(response).to have_http_status(401)
+    expect(JSON.parse(response.body)['message']).to eq('There is a problem with your access token.')
+  end
 end
