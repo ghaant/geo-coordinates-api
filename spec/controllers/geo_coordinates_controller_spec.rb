8c582e25491d19b2c277c30411e7ec878d6723ea
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2020-03-11 23:26:49 +0100

    A first couple of the controller tests.

---
+++ spec/controllers/geo_coordinates_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe GeoCoordinatesController, type: :controller do
+  let!(:incorrect_token) { 'qwerty' }
+  let!(:incorrect_search_value) { 'lkjhgfd' }
+  let!(:ambiguous_search_value) { 'checkpoint charlie' }
+
+  it 'returns 406 with a respective message if no token is provided' do
+    get 'locate'
+
+    expect(response).to have_http_status(406)
+    expect(JSON.parse(response.body)['message']).to eq('Please provide your access token.')
+  end
+
+  it 'returns 406 with a respective message if no search value is provided' do
+    get 'locate', params: { token: 'asdf' }
+
+    expect(response).to have_http_status(406)
+    expect(JSON.parse(response.body)['message']).to eq('Please input the search value: an address or a name of a place you are searching for.')
+  end
+end
